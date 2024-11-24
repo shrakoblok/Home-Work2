1d58a0e66a45a2a1de0a9ba5c512cfa6813c00e6
shrakoblok <99806256+shrakoblok@users.noreply.github.com>
2024-11-24 21:53:28 +0700

    Add files via upload

---
+++ 4.1 Дополнительное практическое задание по модулю/module_2_hard.py
@@ -0,0 +1,15 @@
+import random
+
+first_paste = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+first_number = random.choice(first_paste)
+print(first_number)
+result = []
+for i in range(1, first_number + 1):
+    for j in range(1, first_number):
+        if i == j:
+            continue
+        if first_number % (i + j) == 0:
+            if (str(j) + '.' + str(i)) in result:
+                continue
+            result.append(str(i) + '.' + str(j))
+print(''.join(result).replace('.', ''))
